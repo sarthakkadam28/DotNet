a71f07a6b7a995536a47f1e2cb39c7a8fc241064
sarthakkadam28 <sarthakkadam7777@gmail.com>
2025-07-02 09:30:59 +0530

    new changes in show and remove logic

--- Tflsystem/questionbank.cs
+++ Tflsystem/questionbank.cs
@@ -30,21 +30,38 @@ namespace app
             {
                 if (qt.Title == title)
                 {
+                    qt.Title = thequestion.Title;
                     qt.Option = thequestion.Option;
-                    
+                    qt.Answer = thequestion.Answer;
+                    qt.EvaluationCriteria = thequestion.EvaluationCriteria;
+                    Console.WriteLine("Question updated");
+                    return;
                 }
-                //
+                
             }
          
         }
         public void ShowQuestion(int questionId)
         {
-
+            if (questionId < 0 || questionId >= questions.Count)
+            {
+                Console.WriteLine("Invalid question ID.");
+                return;
+            }
+            Question qt = questions[questionId];
+            qt.Display();
             
         }
         public void RemoveQuestion(int questionId)
         {
             
+            if (questionId < 0 || questionId >= questions.Count)
+            {
+                Console.WriteLine("Invalid question ID.");
+                return;
+            }
+            questions.RemoveAt(questionId);
+            Console.WriteLine("Question removed");
         }
 
     }
