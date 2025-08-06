29abe778f8342fefd881a6849df4db7d954cdd64
sarthakkadam28 <sarthakkadam7777@gmail.com>
2025-08-06 08:51:33 +0530

    adiition of assessment entities

---
+++ Fullstackapplication/Assessment_Entites/AnnualCandidateResult.cs
@@ -0,0 +1,10 @@
+namespace Transflower.TFLAssessment.Entities;
+
+using System;
+public class AnnualCandidateResult
+{
+    public int candidateId { get; set; }
+    public int score { get; set; }
+    public string subjectTitle { get; set; }
+    
+}
