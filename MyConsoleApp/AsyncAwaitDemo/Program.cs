8c19e72a049bd5067725c031fb52bf4ef7fd3db1
sarthakkadam28 <sarthakkadam7777@gmail.com>
2025-08-11 19:20:04 +0530

    AsyncAwaitDemo

--- MyConsoleApp/AsyncAwaitDemo/Program.cs
+++ MyConsoleApp/AsyncAwaitDemo/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Threading.Tasks;
+using AsyncAwaitDemo.Helpers;
+// Console.WriteLine("welcome to transflower..");
+// Task.Delay(10000);
+// console.WriteLine("hello world");
+// Console.WriteLine("press any key to exit");
+
+
+decimal incomeTax = await TaxDeductionManager.CalculateIncomeTax(750000);
+console.WriteLine($"Income Tax : {incomeTax}");
+
+List<Product> products = await TaxDeductionManager.GetAllAvailableProducts();
+console.WriteLine("Available Products");
+foreach (var product in products)
+{
+    console.WriteLine($"Id: {product.Id}, Name: {product.Name}, Price: {product.Price}");
+}
