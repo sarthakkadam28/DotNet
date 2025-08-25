2bc77c492e3ec6a7f93c7ed8ad4a8922beed71f5
sarthakkadam28 <sarthakkadam7777@gmail.com>
2025-08-25 11:48:50 +0530

    Addition of QuestionBankRepository

---
+++ AssessmentSolution/AssessmentLib/Repositories/Implementation/QuestionBankRepository.cs
@@ -0,0 +1,72 @@
+﻿using AssessmentLib.Entities;
+using AssessmentLib.Repositories.Interface;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Sec;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentLib.Repositories.Implementation
+{
+    public class QuestionBankRepository:IQuestionBankRepository
+    { 
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public class QustionBankRepository(IConfiguration configuration)
+         {
+             _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException("connectionString");
+         }
+
+        public Task<List<QuestionTitle>> GetAllQuestion()
+        {
+
+        }
+        public Task<List<SubjetQuestion>> GetQuestionsBySubject(int Id)
+        {
+
+        }
+        public Task<List<QuestionDetails>> GetQuestionsBySubjectAndCriteria(int SubjectId, int CriteriaId)
+        {
+
+
+        }
+        public Task<List<QuestionDetails>> GetQusstionWithSubjectAndCriteria()
+        {
+
+        }
+        public Task<List<Question>> GetQuestions(int TestId)
+        {
+
+
+        }
+        public Task<bool> UpdateAnswer(int Id, char AnswerrKey)
+        {
+
+        }
+        public Task<Question> GetQuestion(int QuestionId)
+        {
+
+        }
+        public Task<bool> UpdateQuestionOption(int Id, Question options)
+        {
+
+        }
+        public Task<bool> UpdateSubjectCriteria(int questionId,Question question)
+        {
+
+        }
+        public Task<bool>InsertQuestion(NewQuestion question)
+        {
+
+        }
+        public Task<bool> GetCriteria(string subject, int QuestiionId)
+        {
+
+        }
+
+    }
+}
---
+++ AssessmentSolution/AssessmentLib/Repositories/Interface/IQuestionBankRepository.cs
@@ -0,0 +1,24 @@
+﻿using AssessmentLib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentLib.Repositories.Interface
+{
+    public class IQuestionBankRepository
+    {
+        public Task<List<QuestionTitle>> GetAllQuestions();
+        public Task<List<SubjectQuestion>> GetQuestionsBySubject(int id);
+        public Task<List<QuestionDetails>> GetQuestionsBySubjectAndCriteria(int SubjectId, int CriteriaId);
+        public Task<List<QuestionDetails>> GetQuestionWithSubjectAndCriteria();
+        public Task<List<Question>> GetQuestions(int TestId);
+        public Task<bool> UpdateAnswer(int id, char AnswerKey);
+        public Task<bool> GetQuestion(int QuestionId);
+        public Task<bool> UpdateSubjectCriteria(int QuestionId, Question question);
+        public Task<bool> UpdateQuestionOptions(int Id, Question options);
+        public Task<bool> InsertQuestion(newQuestion question);
+        public Task<String> GetString(String subject, int QuestionId);
+    }
+}
