a8df2cd056164f7cd30b003ec8e84d9d68b0ab90
sarthakkadam28 <sarthakkadam7777@gmail.com>
2025-09-29 14:34:28 +0530

    Annoymous Method

---
+++ concepts/Annoymous_method/Program.cs
@@ -0,0 +1,32 @@
+﻿//simple code method
+using System;
+
+class Program
+{
+    static void SayHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+
+    static void Main()
+    {
+        SayHello(); // Call the named method
+    }
+}
+// using anonymous method
+
+class Program
+{
+    delegate void MyDelegate();
+
+    static void Main()
+    {
+        // Define the method without a name
+        MyDelegate hello = delegate ()
+        {
+            Console.WriteLine("Hello!");
+        };
+
+        hello(); // Call the anonymous method
+    }
+}
